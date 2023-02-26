411046ab0b4acd3c3ea3829fb79e55812ceeadbe
alper-savas <alpersavas1998@gmail.com>
2023-02-26 13:09:02 +0100

    Add autocorrection and direction information.

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const key1 = config.KEY1;
 const key2 = config.KEY2;
+
 // Select DOM Elements
 const form = document.querySelector(".form");
 const startingEl = document.querySelector(".starting-point");
 const destinationEl = document.querySelector(".destination-point");
 const submitFormEl = document.querySelector(".submit-form");
-// const suggestionEl = document.querySelector(".suggestion");
+const startingDiv = document.querySelector(".starting-div");
+const destinationDiv = document.querySelector(".destination-div");
+const suggestions = document.querySelectorAll(".suggestion");
+const directionInner = document.querySelector(".dir-inner");
 
 let map;
+let narratives = [];
 
 // Load Default Map
 window.onload = () => {
@@ -60,14 +65,54 @@ async function findRoute(start, destination) {
 
   var directions = L.mapquest.directions();
 
-  directions.route({
-    start: start,
-    end: destination,
+  const obj = {
+    locations: [start, destination],
     options: {
       maxRoutes: 2,
       compactResults: true,
     },
-  });
+  };
+
+  const getRoute = () => {
+    const request = new Request(
+      `http://www.mapquestapi.com/directions/v2/route?key=${key1}`,
+      {
+        method: "POST",
+        body: JSON.stringify(obj),
+      }
+    );
+    request.json().then((data) => {
+      directions.route(data);
+    });
+  };
+  getRoute();
+
+  const getNarrative = async () => {
+    const response = await fetch(
+      `http://www.mapquestapi.com/directions/v2/route?key=${key1}`,
+      {
+        method: "POST",
+        body: JSON.stringify(obj),
+      }
+    );
+    const responseObj = await response.json();
+    const narrativeList = responseObj.route.legs[0].maneuvers;
+    narratives = [];
+    narrativeList.forEach((nar) => {
+      narratives.push(nar.narrative);
+    });
+    console.log(narratives);
+
+    let html = ``;
+    narratives.forEach((nar) => {
+      html += `
+      <p class="dir-el">
+          ${nar}
+      </p>`;
+    });
+    directionInner.innerHTML = html;
+  };
+  getNarrative();
 
   directions.setLayerOptions({
     startMarker: {
@@ -95,17 +140,67 @@ async function findRoute(start, destination) {
   });
 }
 
-// startingEl.addEventListener("change", async function () {
-//   let query = startingEl.value;
-//   const search = await fetch(
-//     `http://www.mapquestapi.com/search/v3/prediction?key=vh9aA8AG9GPx7CKivkGYZJfNxbNKGNz2&limit=5&collection=adminArea,poi,address,category,franchise,airport&q=${query}`
-//   );
-//   const sData = await search.json();
-//   let suggestion = sData.results[0].displayString;
-//   suggestionEl.classList.remove("hidden");
-//   suggestionEl.placeholder = suggestion;
-// });
+const suggestStartingPoint = async () => {
+  let query = startingEl.value;
+  const searchCity = await fetch(
+    `https://api.geoapify.com/v1/geocode/autocomplete?text=${query}&type=city&format=json&apiKey=${key2}`
+  );
+  const searchStreet = await fetch(
+    `https://api.geoapify.com/v1/geocode/autocomplete?text=${query}&type=street&format=json&apiKey=${key2}`
+  );
+  const cityData = await searchCity.json();
+  const streetData = await searchStreet.json();
+  if (startingEl.value.length > 2 && cityData && streetData) {
+    startingEl.classList.add("bottom-radius");
+    startingDiv.classList.add("open");
+  }
+  if (startingEl.value.length === 0) {
+    startingEl.classList.remove("bottom-radius");
+    startingDiv.classList.remove("open");
+  }
+  suggestions[0].placeholder = cityData.results[0].formatted;
+  suggestions[1].placeholder = streetData.results[0].formatted;
+  suggestions[2].placeholder = streetData.results[1].formatted;
+};
 
-// suggestionEl.addEventListener("click", () => {
-//   suggestionEl.classList.add("hidden");
-// });
+const suggestDestinationPoint = async () => {
+  let query = destinationEl.value;
+  const searchCity = await fetch(
+    `https://api.geoapify.com/v1/geocode/autocomplete?text=${query}&type=city&format=json&apiKey=${key2}`
+  );
+  const searchStreet = await fetch(
+    `https://api.geoapify.com/v1/geocode/autocomplete?text=${query}&type=street&format=json&apiKey=${key2}`
+  );
+  const cityData = await searchCity.json();
+  const streetData = await searchStreet.json();
+
+  if (destinationEl.value.length > 2 && cityData && streetData) {
+    destinationEl.classList.add("bottom-radius");
+    destinationDiv.classList.add("open");
+  }
+  if (destinationEl.value.length === 0) {
+    destinationEl.classList.remove("bottom-radius");
+    destinationDiv.classList.remove("open");
+  }
+  suggestions[3].placeholder = cityData.results[0].formatted;
+  suggestions[4].placeholder = streetData.results[0].formatted;
+  suggestions[5].placeholder = streetData.results[1].formatted;
+};
+
+// Listen Event
+startingEl.addEventListener("keydown", suggestStartingPoint);
+destinationEl.addEventListener("keydown", suggestDestinationPoint);
+
+suggestions.forEach((el) =>
+  el.addEventListener("click", () => {
+    if (Array.prototype.indexOf.call(suggestions, el) < 3) {
+      startingEl.value = el.placeholder;
+      startingEl.classList.remove("bottom-radius");
+      startingDiv.classList.remove("open");
+    } else {
+      destinationEl.value = el.placeholder;
+      destinationEl.classList.remove("bottom-radius");
+      destinationDiv.classList.remove("open");
+    }
+  })
+);
